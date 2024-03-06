c524c4cec05f91e78722859459e0c452b53e3f16
Alexis Liao <Alexisliao10@hotmail.com>
2024-03-05 22:04:57 -0500

    feat: move files to new folders
    Moved existing files to new folders to improve project organization and structure.

---
+++ src/App/index.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { useLocalStorage } from "./useLocalStorage";
+import { AppUI } from "./AppUI";
+
+const defaultTodos = [
+  { text: "Cortar cebolla", completed: true },
+  { text: "Tomar el curso de intro a React.js", completed: false },
+  { text: "Llorar con la llorona", completed: false },
+];
+
+function App() {
+  const [todos, saveTodos] = useLocalStorage("TODOS_V1", defaultTodos);
+  const [searchValue, setSearchValue] = React.useState("");
+
+  const completedTodos = todos.filter((todo) => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+  const searchedTodos = todos.filter((todo) => {
+    const todoText = todo.text.toLowerCase();
+    const searchText = searchValue.toLowerCase();
+    return todoText.includes(searchText);
+  });
+
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex((todo) => todo.text === text);
+    const isTodoCompleted = newTodos[todoIndex].completed;
+    newTodos[todoIndex].completed = !isTodoCompleted;
+    saveTodos(newTodos);
+  };
+
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex((todo) => todo.text === text);
+    newTodos.splice(todoIndex, 1);
+    saveTodos(newTodos);
+  };
+  return (
+    <AppUI
+      completedTodos={completedTodos}
+      totalTodos={totalTodos}
+      searchValue={searchValue}
+      searchedTodos={searchedTodos}
+      setSearchValue={setSearchValue}
+      completeTodo={completeTodo}
+      deleteTodo={deleteTodo}
+    />
+  );
+}
+
+export default App;
---
+++ src/App/useLocalStorage.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function useLocalStorage(itemName, initialValue) {
+  const localStorageItem = localStorage.getItem(itemName);
+  let parsedItem = JSON.parse(localStorageItem);
+
+  if (!localStorageItem) {
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItem = initialValue;
+  }
+
+  const [item, setItem] = React.useState(parsedItem);
+
+  const saveItem = (newItem) => {
+    localStorage.setItem(itemName, JSON.stringify(newItem));
+    setItem(newItem);
+  };
+
+  return [item, saveItem];
+}
+
+export { useLocalStorage };
