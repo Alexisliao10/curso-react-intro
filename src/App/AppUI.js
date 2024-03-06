2a184ea64c1ad0564fffe6280e79d9752aa27024
Alexis Liao <Alexisliao10@hotmail.com>
2024-03-06 14:07:26 -0500

    feat: added loading skeleton
    Added loading skeleton when retrieving information from the localStorage.

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -4,7 +4,12 @@ import { TodoSearch } from "../TodoSearch";
 import { TodoList } from "../TodoList";
 import { TodoItem } from "../TodoItem";
 import { CreateTodoButton } from "../CreateTodoButton";
+import { TodosLoading } from "../TodosLoading";
+import { TodosError } from "../TodosError";
+import { TodosEmpty } from "../TodosEmpty";
 function AppUI({
+  loading,
+  error,
   completedTodos,
   totalTodos,
   searchValue,
@@ -25,6 +30,16 @@ function AppUI({
       />
 
       <TodoList>
+        {loading && (
+          <>
+            <TodosLoading />
+            <TodosLoading />
+            <TodosLoading />
+          </>
+        )}
+        {error && <TodosError />}
+        {!loading && searchedTodos.length === 0 && <TodosEmpty />}
+
         {searchedTodos.map((todo) => (
           <TodoItem
             key={todo.text}
